f15451e1b2e4286206a0ee4565ec273ce7482932
arupay <arupay11374@gmail.com>
2022-04-18 12:04:10 -0400

    done

--- M2/api-calls-fetch/activity/pokemon-api/script.js
+++ M2/api-calls-fetch/activity/pokemon-api/script.js
@@ -1,3 +1,20 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
-const form = document.querySelector('form');
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const pokemon = getPokemonByID(event.target.id.value);
+  return pokemon;
+});
+
+const getPokemonByID = (num) => {
+  fetch(BASE_URL + num)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
--- M2/api-calls-fetch/script.js
+++ M2/api-calls-fetch/script.js
@@ -1,7 +1,51 @@
-const BASE_URL = 'https://randomuser.me/api';
+const BASE_URL = "https://randomuser.me/api";
+// create a function to display dom elements from the api response
+
+const displayUser = ({ results }) => {
+  console.log(results);
+  const [person] = results;
+  const { title, first, last } = person.name;
+  const { email, picture } = person;
+  console.log(title, first, last, email);
+
+  const section = document.createElement("section");
+  section.classList.add("card");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = `${title} ${first} ${last}`;
+
+  const img = document.createElement("img");
+  img.setAttribute("src", picture.large);
+
+  const p = document.createElement("p");
+  p.textContent = email;
+
+  section.append(img, h2, p);
+  document.querySelector(".people").append(section);
+};
+
+// create a function to display errors from the api response
+
+const displayError = (e) => {
+  const section = document.querySelector("section.error");
+  section.getElementsByClassName.display = "block";
+
+  const p = document.createElement("p");
+  p.textContent = "Something went wrong";
+
+  const eMsg = document.createElement("p");
+  eMsg.textContent = error;
+};
 
 // Fetch data from base url using fetch and promises
 
-// create a function to display dom elements from the api response
+const button = document.querySelector("button");
 
-// create a function to display errors from the api response
+button.addEventListener("click", () => {
+  fetch(BASE_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then(displayUser)
+    .catch(displayError);
+});
